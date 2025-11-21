76f0e041d96a952c3e2008636e42f295d76f099a
afcj8 <cruz.junior@escolar.ifrn.edu.br>
2025-11-21 11:56:27 -0300

    feat: service de update usuario

--- task_manager_api/services/usuario_service.py
+++ task_manager_api/services/usuario_service.py
@@ -27,3 +27,33 @@ class UsuarioService:
         usuario.senha = criar_hash_senha(usuario.senha)
         novo_usuario = self.usuario_repository.add_usuario(usuario)
         return novo_usuario
+    
+    def update_usuario(self, usuario_id: int, usuario_atualizado: Usuario, usuario_logado: Usuario) -> Usuario:
+
+        usuario_existente = self.usuario_repository.get_usuario_por_id(usuario_id)
+
+        if not usuario_existente:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Usuário não encontrado"
+            )
+
+        if usuario_existente.id != usuario_logado.id:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="Você não tem permissão para atualizar este usuário."
+            )
+
+        if usuario_atualizado.username and usuario_atualizado.username != usuario_existente.username:
+            self.checar_usuario_existente(usuario_atualizado.username, usuario_existente.email)
+            usuario_existente.username = usuario_atualizado.username
+        
+        if usuario_atualizado.email and usuario_atualizado.email != usuario_existente.email:
+            self.checar_usuario_existente(usuario_existente.username, usuario_atualizado.email)
+            usuario_existente.email = usuario_atualizado.email
+        
+        self.usuario_repository.db_session.add(usuario_existente)
+        self.usuario_repository.db_session.commit()
+        self.usuario_repository.db_session.refresh(usuario_existente)
+
+        return usuario_existente
