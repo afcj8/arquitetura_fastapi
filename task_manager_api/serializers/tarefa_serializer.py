8c6bd2a569dbd4344e15848d2e2e82f2fd03eb33
afcj8 <cruz.junior@escolar.ifrn.edu.br>
2025-11-21 17:31:03 -0300

    feat: serializer de tarefa

---
+++ task_manager_api/serializers/tarefa_serializer.py
@@ -0,0 +1,24 @@
+from typing import Optional
+from datetime import datetime
+from pydantic import BaseModel
+
+class TarefaRequest(BaseModel):
+    """Representa o modelo de criação da tarefa"""
+    
+    titulo: str
+    descricao: Optional[str] = None
+    status: str
+    prioridade: str
+    usuario_id: int
+    data_criacao: Optional[datetime] = datetime.now()
+
+class TarefaResponse(BaseModel):
+    """Representa o modelo de resposta da tarefa"""
+    
+    id: int
+    titulo: str
+    descricao: Optional[str] = None
+    status: str
+    prioridade: str
+    usuario_id: int
+    data_criacao: datetime
